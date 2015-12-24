5db1335d479b0e86f59d3b5323d5d773ea7490d8
morzhovets <morzhovets@gmail.com>
2015-12-25 02:42:37 +0500

    LibcxxTreeSetTester

--- test/tests/LibcxxTreeSetTester.cpp
+++ test/tests/LibcxxTreeSetTester.cpp
@@ -19,25 +19,15 @@
 namespace
 {
 
-#define _LIBCPP_DEBUG 1
-#define _LIBCPP_DEBUG_LEVEL 1
-
 #define LIBCXX_TEST_PREFIX "libcxx_test_set"
-struct LibcxxTreeSetSettings : public momo::TreeSetSettings
-{
-	static const momo::CheckMode checkMode = momo::CheckMode::exception;
-};
 template<typename TKey,
 	typename TLessFunc = std::less<TKey>,
 	typename TAllocator = std::allocator<TKey>>
 using set = momo::stdish::set<TKey, TLessFunc, TAllocator,
 	momo::TreeSet<TKey, momo::TreeTraitsStd<TKey, TLessFunc>,
-		momo::MemManagerStd<TAllocator>, momo::TreeSetItemTraits<TKey>, LibcxxTreeSetSettings>>;
+		momo::MemManagerStd<TAllocator>, momo::TreeSetItemTraits<TKey>, momo::TreeSetSettings>>;
 #include "LibcxxSetTests.h"
 #undef LIBCXX_TEST_PREFIX
-
-#undef _LIBCPP_DEBUG
-#undef _LIBCPP_DEBUG_LEVEL
 
 } // namespace
 
