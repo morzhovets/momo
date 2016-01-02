5822c8e9084f88417d5ecc07fcf2eb9ecf1bde4d
morzhovets <morzhovets@gmail.com>
2016-01-02 22:50:20 +0500

    License

--- momo/HashMultiMap.h
+++ momo/HashMultiMap.h
@@ -1,5 +1,8 @@
 /**********************************************************\
 
+  This file is distributed under the MIT License.
+  See accompanying file LICENSE for details.
+
   momo/HashMultiMap.h
 
   namespace momo:
--- momo/HashSet.h
+++ momo/HashSet.h
@@ -1,5 +1,8 @@
 /**********************************************************\
 
+  This file is distributed under the MIT License.
+  See accompanying file LICENSE for details.
+
   momo/HashSet.h
 
   namespace momo:
