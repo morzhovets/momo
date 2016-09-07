7c9133f99d2a1e6508c600305fbefbcd1e8fac16
Dmitry Bochkarev <morzhovets@gmail.com>
2016-09-08 00:55:14 +0500

    SimpleDataTester

--- test/tests/SimpleDataTester.cpp
+++ test/tests/SimpleDataTester.cpp
@@ -20,9 +20,55 @@
 
 class SimpleDataTester
 {
+private:
+	struct Struct
+	{
+		int intCol;
+		double dblCol;
+		std::string strCol;
+	};
+
 public:
 	static void TestAll()
 	{
+		std::cout << "momo::experimental::DataColumnListStatic: " << std::flush;
+		TestData(momo::experimental::DataColumnListStatic<Struct>());
+		std::cout << "ok" << std::endl;
+
+		std::cout << "momo::experimental::DataColumnList: " << std::flush;
+		typedef momo::experimental::DataColumnList<momo::experimental::DataColumnTraits<Struct>> Columns;
+		Columns columns(&Struct::strCol, &Struct::intCol, &Struct::dblCol);
+		TestData(columns);
+		std::cout << "ok" << std::endl;
+	}
+
+	template<typename DataColumnList>
+	static void TestData(const DataColumnList& columns)
+	{
+		typedef momo::experimental::DataTable<DataColumnList> DataTable;
+		typedef typename DataTable::Row DataRow;
+
+		DataTable table(columns);
+
+		assert(table.AddUniqueHashIndex(&Struct::intCol, &Struct::strCol));
+		assert(table.AddMultiHashIndex(&Struct::intCol));
+		assert(table.AddMultiHashIndex(&Struct::strCol));
+
+		for (size_t i = 0; i < 1024; ++i)
+		{
+			DataRow row = table.NewRow(&Struct::intCol, (int)i / 2);
+			row[&Struct::strCol] = (i % 2 == 0) ? "0" : "1";
+			row[&Struct::dblCol] = (double)i / 2;
+			table.AddRow(std::move(row));
+		}
+		assert(table.GetCount() == 1024);
+
+		assert(table.HasUniqueHashIndex(&Struct::intCol, &Struct::strCol));
+		assert(table.HasMultiHashIndex(&Struct::intCol));
+		assert(table.RemoveUniqueHashIndex(&Struct::intCol, &Struct::strCol));
+		assert(table.RemoveMultiHashIndex(&Struct::intCol));
+		assert(table.AddUniqueHashIndex(&Struct::intCol, &Struct::strCol));
+		assert(table.AddMultiHashIndex(&Struct::intCol));
 	}
 };
 
