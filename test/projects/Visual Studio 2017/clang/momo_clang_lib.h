a8c01ba6480e6182ede58da70e90ac50ce45508a
Dmitry Bochkarev <morzhovets@gmail.com>
2017-03-31 21:58:20 +0500

    Visual Studio 2017 + Clang

---
+++ test/projects/Visual Studio 2017/clang/momo_clang_lib.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// This is an example of a class exported from the momo_clang_lib.lib
+class Cmomo_clang_lib
+{
+public:
+    Cmomo_clang_lib();
+    // TODO: add your methods here.
+};
+
+// This is an example of an exported variable
+extern int nmomo_clang_lib;
+
+// This is an example of an exported function.
+int fnmomo_clang_lib(void);
