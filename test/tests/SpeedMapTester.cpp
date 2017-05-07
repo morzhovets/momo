75b5c08e28202612fc6c7970966f7315c0858a19
Dmitry Bochkarev <morzhovets@gmail.com>
2017-05-07 19:15:29 +0500

    SpeedMapTester

--- test/tests/SpeedMapTester.cpp
+++ test/tests/SpeedMapTester.cpp
@@ -24,57 +24,103 @@
 #include <map>
 #include <random>
 
-class SpeedMapTesterKey
+class SpeedMapKey
 {
 public:
-	SpeedMapTesterKey(SpeedMapTesterKey* ptr, uint32_t value) MOMO_NOEXCEPT
-		: mPtr(ptr),
-		mValue(value)
+	explicit SpeedMapKey(uint32_t* ptr) MOMO_NOEXCEPT
+		: mPtr(ptr)
 	{
 	}
 
-	uint32_t GetPtrValue() const MOMO_NOEXCEPT
+	uint32_t GetInt() const MOMO_NOEXCEPT
 	{
-		return mPtr->mValue;
+		return *mPtr;
 	}
 
-	bool operator==(const SpeedMapTesterKey& key) const MOMO_NOEXCEPT
+	bool operator==(const SpeedMapKey& key) const MOMO_NOEXCEPT
 	{
-		return GetPtrValue() == key.GetPtrValue();
+		return GetInt() == key.GetInt();
 	}
 
-	bool operator<(const SpeedMapTesterKey& key) const MOMO_NOEXCEPT
+	bool operator<(const SpeedMapKey& key) const MOMO_NOEXCEPT
 	{
-		return GetPtrValue() < key.GetPtrValue();
-	}
-
-	friend void swap(SpeedMapTesterKey& key1, SpeedMapTesterKey& key2) MOMO_NOEXCEPT
-	{
-		std::swap(key1.mPtr, key2.mPtr);
+		return GetInt() < key.GetInt();
 	}
 
 private:
-	SpeedMapTesterKey* mPtr;
-	uint32_t mValue;
+	uint32_t* mPtr;
 };
 
 namespace std
 {
 	template<>
-	struct hash<SpeedMapTesterKey>
+	struct hash<SpeedMapKey>
 	{
-		size_t operator()(const SpeedMapTesterKey& key) const MOMO_NOEXCEPT
+		size_t operator()(const SpeedMapKey& key) const MOMO_NOEXCEPT
 		{
-			return std::hash<uint32_t>()(key.GetPtrValue());
+			return std::hash<uint32_t>()(key.GetInt());
 		}
 	};
 }
+
+template<typename Key>
+class SpeedMapKeys;
+
+template<>
+class SpeedMapKeys<SpeedMapKey> : public momo::Array<SpeedMapKey>
+{
+public:
+	SpeedMapKeys(size_t count, std::mt19937& random)
+	{
+		mInts.Reserve(count);
+		Reserve(count);
+		for (size_t i = 0; i < count; ++i)
+		{
+			mInts.AddBackNogrow(random());
+			AddBackNogrow(SpeedMapKey(mInts.GetItems() + i));
+		}
+		std::shuffle(GetBegin(), GetEnd(), random);
+	}
+
+private:
+	momo::Array<uint32_t> mInts;
+};
+
+template<>
+class SpeedMapKeys<uint32_t> : public momo::Array<uint32_t>
+{
+public:
+	SpeedMapKeys(size_t count, std::mt19937& random)
+	{
+		Reserve(count);
+		for (size_t i = 0; i < count; ++i)
+			AddBackNogrow(random());
+	}
+};
+
+template<>
+class SpeedMapKeys<std::string> : public momo::Array<std::string>
+{
+public:
+	SpeedMapKeys(size_t count, std::mt19937& random)
+	{
+		Reserve(count);
+		for (size_t i = 0; i < count; ++i)
+		{
+			std::stringstream sstream;
+			sstream << random();
+			AddBackNogrow(sstream.str());
+		}
+	}
+};
 
 template<typename TKey>
 class SpeedMapTester
 {
 public:
 	typedef TKey Key;
+	typedef uint32_t Value;
+	typedef SpeedMapKeys<Key> Keys;
 
 private:
 	class Timer
@@ -107,21 +153,26 @@ private:
 	};
 
 public:
-	explicit SpeedMapTester(size_t count, std::ostream& stream = std::cout);
+	explicit SpeedMapTester(size_t count, std::ostream& stream = std::cout)
+		: mRandom(),
+		mKeys(count, mRandom),
+		mStream(stream)
+	{
+	}
 
 	template<typename Allocator>
 	void TestStdUnorderedMap(const std::string& mapTitle)
 	{
-		typedef std::unordered_map<Key, uint32_t, std::hash<Key>, std::equal_to<Key>, Allocator> Map;
+		typedef std::unordered_map<Key, Value, std::hash<Key>, std::equal_to<Key>, Allocator> Map;
 		pvTestMap<Map>(mapTitle);
 	}
 
 	template<typename HashBucket>
 	void TestHashMap(const std::string& mapTitle)
 	{
-		typedef momo::stdish::unordered_map<Key, uint32_t, std::hash<Key>, std::equal_to<Key>,
-			std::allocator<std::pair<const Key, uint32_t>>,
-			momo::HashMap<Key, uint32_t,
+		typedef momo::stdish::unordered_map<Key, Value, std::hash<Key>, std::equal_to<Key>,
+			std::allocator<std::pair<const Key, Value>>,
+			momo::HashMap<Key, Value,
 				momo::HashTraitsStd<Key, std::hash<Key>, std::equal_to<Key>, HashBucket>>> Map;
 		pvTestMap<Map>(mapTitle);
 	}
@@ -129,28 +180,28 @@ public:
 	template<typename Allocator>
 	void TestStdMap(const std::string& mapTitle)
 	{
-		typedef std::map<Key, uint32_t, std::less<Key>, Allocator> Map;
+		typedef std::map<Key, Value, std::less<Key>, Allocator> Map;
 		pvTestMap<Map>(mapTitle);
 	}
 
 	template<typename TreeNode>
 	void TestTreeMap(const std::string& mapTitle)
 	{
-		typedef momo::stdish::map<Key, uint32_t, std::less<Key>, std::allocator<std::pair<const Key, uint32_t>>,
-			momo::TreeMap<Key, uint32_t, momo::TreeTraitsStd<Key, std::less<Key>, TreeNode>>> Map;
+		typedef momo::stdish::map<Key, Value, std::less<Key>, std::allocator<std::pair<const Key, Value>>,
+			momo::TreeMap<Key, Value, momo::TreeTraitsStd<Key, std::less<Key>, TreeNode>>> Map;
 		pvTestMap<Map>(mapTitle);
 	}
 
 	void TestAll()
 	{
-		TestStdUnorderedMap<std::allocator<std::pair<const Key, uint32_t>>>("std::unordered_map");
+		TestStdUnorderedMap<std::allocator<std::pair<const Key, Value>>>("std::unordered_map");
 		TestHashMap<momo::HashBucketLimP<>>("HashMapLimP");
 		TestHashMap<momo::HashBucketOneI1>("HashMapOneI1");
 		//TestHashMap<momo::HashBucketLimP1<>>("HashMapLimP1");
 		//TestHashMap<momo::HashBucketUnlimP<>>("HashMapUnlimP");
 		//TestHashMap<momo::HashBucketLim4<>>("HashMapLim4");
 
-		TestStdMap<momo::stdish::pool_allocator<std::pair<const Key, uint32_t>>>("std::map + pool_allocator");
+		TestStdMap<momo::stdish::pool_allocator<std::pair<const Key, Value>>>("std::map + pool_allocator");
 		TestTreeMap<momo::TreeNode<32, 4, momo::MemPoolParams<>, true>>("TreeNodeSwp");
 		TestTreeMap<momo::TreeNode<32, 4, momo::MemPoolParams<>, false>>("TreeNodePrm");
 	}
@@ -160,21 +211,20 @@ private:
 	void pvTestMap(const std::string& mapTitle)
 	{
 		Map map;
-		std::mt19937 mt;
 		{
 			Timer timer(mapTitle + " insert: ", mStream);
 			for (const Key& key : mKeys)
 				map[key] = 0;
 		}
 
-		std::shuffle(mKeys.GetBegin(), mKeys.GetEnd(), mt);
+		std::shuffle(mKeys.GetBegin(), mKeys.GetEnd(), mRandom);
 		{
 			Timer timer(mapTitle + " find: ", mStream);
 			for (const Key& key : mKeys)
 				map[key] = 0;
 		}
 
-		std::shuffle(mKeys.GetBegin(), mKeys.GetEnd(), mt);
+		std::shuffle(mKeys.GetBegin(), mKeys.GetEnd(), mRandom);
 		{
 			Timer timer(mapTitle + " erase: ", mStream);
 			for (const Key& key : mKeys)
@@ -183,44 +233,10 @@ private:
 	}
 
 private:
-	momo::Array<Key> mKeys;
+	std::mt19937 mRandom;
+	Keys mKeys;
 	std::ostream& mStream;
 };
-
-template<>
-SpeedMapTester<SpeedMapTesterKey>::SpeedMapTester(size_t count, std::ostream& stream)
-	: mStream(stream)
-{
-	std::mt19937 mt;
-	mKeys.Reserve(count);
-	for (size_t i = 0; i < count; ++i)
-		mKeys.AddBack(SpeedMapTesterKey(mKeys.GetItems() + i, mt()));
-	std::shuffle(mKeys.GetBegin(), mKeys.GetEnd(), mt);
-}
-
-template<>
-SpeedMapTester<uint32_t>::SpeedMapTester(size_t count, std::ostream& stream)
-	: mStream(stream)
-{
-	std::mt19937 mt;
-	mKeys.Reserve(count);
-	for (size_t i = 0; i < count; ++i)
-		mKeys.AddBack(mt());
-}
-
-template<>
-SpeedMapTester<std::string>::SpeedMapTester(size_t count, std::ostream& stream)
-	: mStream(stream)
-{
-	std::mt19937 mt;
-	mKeys.Reserve(count);
-	for (size_t i = 0; i < count; ++i)
-	{
-		std::stringstream sstream;
-		sstream << mt();
-		mKeys.AddBack(sstream.str());
-	}
-}
 
 void TestSpeedMap()
 {
@@ -230,9 +246,9 @@ void TestSpeedMap()
 	const size_t count = 1 << 12;
 #endif
 
-	SpeedMapTester<SpeedMapTesterKey> speedMapTester(count, std::cout);
-	//SpeedMapTester<uint32_t> speedMapTester(count, std::cout);
-	//SpeedMapTester<std::string> speedMapTester(count, std::cout);
+	SpeedMapTester<SpeedMapKey> speedMapTester(count);
+	//SpeedMapTester<uint32_t> speedMapTester(count);
+	//SpeedMapTester<std::string> speedMapTester(count);
 
 	for (size_t i = 0; i < 3; ++i)
 	{
