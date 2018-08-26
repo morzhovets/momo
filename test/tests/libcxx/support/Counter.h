28296fd8f004cccf7b85a3361994fb5ca4866134
Dmitry Bochkarev <morzhovets@gmail.com>
2018-08-27 00:02:14 +0500

    tests/libcxx/support

--- test/tests/libcxx/support/Counter.h
+++ test/tests/libcxx/support/Counter.h
@@ -13,7 +13,7 @@
 #include <functional> // for std::hash
 
 struct Counter_base { static int gConstructed; };
-    
+
 template <typename T>
 class Counter : public Counter_base
 {
@@ -21,13 +21,13 @@ public:
     Counter() : data_()                             { ++gConstructed; }
     Counter(const T &data) : data_(data)            { ++gConstructed; }
     Counter(const Counter& rhs) : data_(rhs.data_)  { ++gConstructed; }
-    Counter& operator=(const Counter& rhs)          { ++gConstructed; data_ = rhs.data_; return *this; }
+    Counter& operator=(const Counter& rhs)          { data_ = rhs.data_; return *this; }
 #ifndef _LIBCPP_HAS_NO_RVALUE_REFERENCES
     Counter(Counter&& rhs) : data_(std::move(rhs.data_))  { ++gConstructed; }
     Counter& operator=(Counter&& rhs) { ++gConstructed; data_ = std::move(rhs.data_); return *this; }
 #endif
     ~Counter() { --gConstructed; }
-    
+
     const T& get() const {return data_;}
 
     bool operator==(const Counter& x) const {return data_ == x.data_;}
@@ -43,9 +43,11 @@ namespace std {
 
 template <class T>
 struct hash<Counter<T> >
-    : public std::unary_function<Counter<T>, std::size_t>
 {
-    std::size_t operator()(const Counter<T>& x) const {return std::hash<T>(x.get());}
+    typedef Counter<T> argument_type;
+    typedef std::size_t result_type;
+
+    std::size_t operator()(const Counter<T>& x) const {return std::hash<T>()(x.get());}
 };
 }
 
