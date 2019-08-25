20e11b232044961e8827c05166ab278b8d843275
Dmitry Bochkarev <morzhovets@gmail.com>
2019-08-25 21:24:52 +0500

    UIntMath::Log2

--- momo/Utility.h
+++ momo/Utility.h
@@ -251,50 +251,54 @@ namespace internal
 			return result;
 		}
 
-		static UInt Log2(UInt value) MOMO_NOEXCEPT;
+		static UInt Log2(UInt value) MOMO_NOEXCEPT
+		{
+			return pvLog2(value);
+		}
+
+	private:
+		template<size_t size = sizeof(UInt)>
+		static typename std::enable_if<size == 4, UInt>::type pvLog2(UInt value) MOMO_NOEXCEPT
+		{
+			static const UInt tab32[32] =
+			{
+				 0,  9,  1, 10, 13, 21,  2, 29,
+				11, 14, 16, 18, 22, 25,  3, 30,
+				 8, 12, 20, 28, 15, 17, 24,  7,
+				19, 27, 23,  6, 26,  5,  4, 31,
+			};
+			value |= value >> 1;
+			value |= value >> 2;
+			value |= value >> 4;
+			value |= value >> 8;
+			value |= value >> 16;
+			return tab32[(value * (UInt)0x07C4ACDD) >> 27];
+		}
+
+		template<size_t size = sizeof(UInt)>
+		static typename std::enable_if<size == 8, UInt>::type pvLog2(UInt value) MOMO_NOEXCEPT
+		{
+			static const UInt tab64[64] =
+			{
+				63,  0, 58,  1, 59, 47, 53,  2,
+				60, 39, 48, 27, 54, 33, 42,  3,
+				61, 51, 37, 40, 49, 18, 28, 20,
+				55, 30, 34, 11, 43, 14, 22,  4,
+				62, 57, 46, 52, 38, 26, 32, 41,
+				50, 36, 17, 19, 29, 10, 13, 21,
+				56, 45, 25, 31, 35, 16,  9, 12,
+				44, 24, 15,  8, 23,  7,  6,  5,
+			};
+			value |= value >> 1;
+			value |= value >> 2;
+			value |= value >> 4;
+			value |= value >> 8;
+			value |= value >> 16;
+			value |= value >> 32;
+			value -= value >> 1;
+			return tab64[(value * (UInt)0x07EDD5E59A4E28C2) >> 58];
+		}
 	};
-
-	template<>
-	inline uint32_t UIntMath<uint32_t>::Log2(uint32_t value) MOMO_NOEXCEPT
-	{
-		static const uint32_t tab32[32] =
-		{
-			 0,  9,  1, 10, 13, 21,  2, 29,
-			11, 14, 16, 18, 22, 25,  3, 30,
-			 8, 12, 20, 28, 15, 17, 24,  7,
-			19, 27, 23,  6, 26,  5,  4, 31,
-		};
-		value |= value >> 1;
-		value |= value >> 2;
-		value |= value >> 4;
-		value |= value >> 8;
-		value |= value >> 16;
-		return tab32[(value * (uint32_t)0x07C4ACDD) >> 27];
-	}
-
-	template<>
-	inline uint64_t UIntMath<uint64_t>::Log2(uint64_t value) MOMO_NOEXCEPT
-	{
-		static const uint64_t tab64[64] =
-		{
-			63,  0, 58,  1, 59, 47, 53,  2,
-			60, 39, 48, 27, 54, 33, 42,  3,
-			61, 51, 37, 40, 49, 18, 28, 20,
-			55, 30, 34, 11, 43, 14, 22,  4,
-			62, 57, 46, 52, 38, 26, 32, 41,
-			50, 36, 17, 19, 29, 10, 13, 21,
-			56, 45, 25, 31, 35, 16,  9, 12,
-			44, 24, 15,  8, 23,  7,  6,  5,
-		};
-		value |= value >> 1;
-		value |= value >> 2;
-		value |= value >> 4;
-		value |= value >> 8;
-		value |= value >> 16;
-		value |= value >> 32;
-		value -= value >> 1;
-		return tab64[(value * (uint64_t)0x07EDD5E59A4E28C2) >> 58];
-	}
 }
 
 } // namespace momo
