29274ddfd2083e27225ad1f04f5c3e0eed7d847b
Dmitry Bochkarev <morzhovets@gmail.com>
2019-11-18 10:22:35 +0500

    Version.h

---
+++ momo/Version.h
@@ -0,0 +1,18 @@
+/**********************************************************\
+
+  This file is distributed under the MIT License.
+  See https://github.com/morzhovets/momo/blob/master/LICENSE
+  for details.
+
+  momo/Version.h
+
+  macros:
+    MOMO_VERSION_MAJOR
+    MOMO_VERSION_MINOR
+
+\**********************************************************/
+
+#pragma once
+
+#define MOMO_VERSION_MAJOR 3
+#define MOMO_VERSION_MINOR 0
