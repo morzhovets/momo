4ab292f55e9c5d42bc82dc51754bf830be5dfd3f
morzhovets <morzhovets@gmail.com>
2019-12-22 18:21:32 +0500

    SimpleArrayTester

--- test/tests/SimpleArrayTester.cpp
+++ test/tests/SimpleArrayTester.cpp
@@ -26,76 +26,128 @@ class SimpleArrayTester
 public:
 	static void TestAll()
 	{
-		std::cout << "momo::Array (1): " << std::flush;
-		typedef momo::Array<std::string> Array1;
-		TestStrArray<Array1>();
+		std::cout << "momo::Array<std::string>: " << std::flush;
+		TestStrArray<momo::Array<std::string>>();
 		std::cout << "ok" << std::endl;
 
-		std::cout << "momo::Array (2): " << std::flush;
-		typedef momo::Array<std::string, momo::MemManagerC,
-			momo::ArrayItemTraits<std::string, momo::MemManagerC>,
-			momo::ArraySettings<std::is_nothrow_move_constructible<std::string>::value ? 2 : 0>> Array2;
-		TestStrArray<Array2>();
+		std::cout << "momo::ArrayIntCap<..., std::string>: " << std::flush;
+		TestStrArray<momo::ArrayIntCap<std::is_nothrow_move_constructible<std::string>::value ? 2 : 0,
+			std::string>>();
 		std::cout << "ok" << std::endl;
 
-		std::cout << "momo::SegmentedArray: " << std::flush;
-		typedef momo::SegmentedArray<std::string> SegmentedArray;
-		TestStrArray<SegmentedArray>();
+		std::cout << "momo::SegmentedArray<std::string>: " << std::flush;
+		TestStrArray<momo::SegmentedArray<std::string>>();
 		std::cout << "ok" << std::endl;
 
-		std::cout << "momo::SegmentedArraySqrt: " << std::flush;
-		typedef momo::SegmentedArraySqrt<std::string> SegmentedArraySqrt;
-		TestStrArray<SegmentedArraySqrt>();
+		std::cout << "momo::SegmentedArraySqrt<std::string>: " << std::flush;
+		TestStrArray<momo::SegmentedArraySqrt<std::string>>();
+		std::cout << "ok" << std::endl;
+
+		std::cout << "momo::Array<size_t, momo::MemManagerCpp>: " << std::flush;
+		TestIntArray<momo::Array<size_t, momo::MemManagerCpp>>();
+		std::cout << "ok" << std::endl;
+
+		std::cout << "momo::Array<size_t, momo::MemManagerC>: " << std::flush;
+		TestIntArray<momo::Array<size_t, momo::MemManagerC>>();
+		std::cout << "ok" << std::endl;
+
+#ifdef MOMO_USE_MEM_MANAGER_WIN
+		std::cout << "momo::Array<size_t, momo::MemManagerWin>: " << std::flush;
+		TestIntArray<momo::Array<size_t, momo::MemManagerWin>>();
+		std::cout << "ok" << std::endl;
+#endif
+
+		std::cout << "momo::ArrayIntCap<4, size_t>: " << std::flush;
+		TestIntArray<momo::ArrayIntCap<4, size_t>>();
+		std::cout << "ok" << std::endl;
+
+		std::cout << "momo::SegmentedArray<size_t>: " << std::flush;
+		TestIntArray<momo::SegmentedArray<size_t>>();
+		std::cout << "ok" << std::endl;
+
+		std::cout << "momo::SegmentedArraySqrt<size_t>: " << std::flush;
+		TestIntArray<momo::SegmentedArraySqrt<size_t>>();
 		std::cout << "ok" << std::endl;
 	}
 
 	template<typename Array>
 	static void TestStrArray()
 	{
-		Array ar;
-		ar = Array(2, "s");
-		ar = Array(1);
+		Array ar = Array::CreateCrt(1, [] (void* ptr) { ::new(ptr) std::string(); });
+
 		std::string s1 = "s1";
 		ar.AddBack(s1);
 		ar.AddBack("s2");
 		ar.AddBackCrt([] (void* ptr) { ::new(ptr) std::string("s3"); });
+
 		ar.Reserve(10);
 		assert(ar.GetCapacity() >= 10);
+
 		assert(ar.Contains("s3"));
 		assert(!ar.Contains("s"));
+
 		ar.AddBackNogrow(s1);
 		ar.AddBackNogrow("s2");
 		ar.AddBackNogrowCrt([] (void* ptr) { ::new(ptr) std::string("s3"); });
+
 		ar.Insert(0, s1);
 		ar.Insert(1, "s2");
 		ar.Insert(3, ar[0]);
 		ar.Insert(3, std::move(ar[3]));
 		ar.Insert(3, 2, ar[0]);
+
 		ar.SetCount(14);
 		ar.SetCount(20, "s");
+
 		ar.Remove(3, 7);
 		ar.RemoveBack(5);
-		ar = ar;
-		ar = std::move(ar);
+
 		ar = Array(std::make_move_iterator(ar.GetBegin()) + 3,
 			std::make_move_iterator(ar.GetEnd()));
 		assert(ar.GetCount() == 5);
+
 		ar.SetCount(4);
 		ar.Shrink();
+
 		//ar.Insert(1, 2, std::make_move_iterator(ar.GetBegin()));
 		ar.Insert(1, std::move(ar[0]));
 		ar.Insert(2, std::move(ar[2]));
+
 		assert(ar[0] == "");
 		assert(ar[1] == "s1");
 		assert(ar[2] == "s2");
 		assert(ar[3] == "");
 		assert(ar[4] == "s3");
 		assert(ar.GetBackItem() == "");
+
 		ar.Clear();
 		assert(ar.IsEmpty());
+		ar.Shrink();
+
 		ar.AddBack(std::move(s1));
-		for (std::string& s : ar)
+		ar.Insert(0, 3, ar[0]);
+		assert(ar.GetCount() == 4);
+
+		Array ar2;
+		ar2 = ar;
+		for (std::string& s : ar2)
 			assert(s == "s1");
+	}
+
+	template<typename Array>
+	static void TestIntArray()
+	{
+		Array ar;
+		static size_t count = 20000;
+
+		for (size_t i = 0; i < count; ++i)
+			ar.AddBack(i);
+
+		for (size_t i = 0; i < count; ++i)
+			assert(ar[i] == i);
+
+		ar.Clear(true);
+		assert(ar.IsEmpty());
 	}
 };
 
