ac9f28b77b8564a16e8f35f5c705fa6e1ec8d139
morzhovets <morzhovets@gmail.com>
2019-12-29 18:40:02 +0500

    Hash tests

--- test/tests/LibcxxHashMapTesterOpen8.cpp
+++ test/tests/LibcxxHashMapTesterOpen8.cpp
@@ -18,9 +18,28 @@
 #include "../../momo/Utility.h"
 #include "../../momo/details/HashBucketOpen8.h"
 
-#define LIBCXX_TEST_BUCKET momo::HashBucketOpen8
-#define LIBCXX_TEST_BUCKET_NAME "open8"
+//#define LIBCXX_TEST_BUCKET momo::HashBucketOpen8
+//#define LIBCXX_TEST_BUCKET_NAME "open8"
 
-#include "LibcxxHashMapTester.h"
+//#include "LibcxxHashMapTester.h"
+
+#include "LibcxxTester.h"
+
+#include "../../momo/stdish/unordered_map.h"
+
+namespace
+{
+
+#define LIBCXX_TEST_PREFIX "libcxx_test_unordered_map_open"
+template<typename TKey, typename TMapped,
+	typename THashFunc = std::hash<TKey>,
+	typename TEqualFunc = std::equal_to<TKey>,
+	typename TAllocator = std::allocator<std::pair<const TKey, TMapped>>>
+using unordered_map = momo::stdish::unordered_map_open<TKey, TMapped,
+	THashFunc, TEqualFunc, TAllocator>;
+#include "LibcxxUnorderedMapTests.h"
+#undef LIBCXX_TEST_PREFIX
+
+} // namespace
 
 #endif // TEST_LIBCXX_HASH_MAP
--- test/tests/LibcxxHashSetTesterOpen8.cpp
+++ test/tests/LibcxxHashSetTesterOpen8.cpp
@@ -18,9 +18,27 @@
 #include "../../momo/Utility.h"
 #include "../../momo/details/HashBucketOpen8.h"
 
-#define LIBCXX_TEST_BUCKET momo::HashBucketOpen8
-#define LIBCXX_TEST_BUCKET_NAME "open8"
+//#define LIBCXX_TEST_BUCKET momo::HashBucketOpen8
+//#define LIBCXX_TEST_BUCKET_NAME "open8"
 
-#include "LibcxxHashSetTester.h"
+//#include "LibcxxHashSetTester.h"
+
+#include "LibcxxTester.h"
+
+#include "../../momo/stdish/unordered_set.h"
+
+namespace
+{
+
+#define LIBCXX_TEST_PREFIX "libcxx_test_unordered_set_open"
+template<typename TKey,
+	typename THashFunc = std::hash<TKey>,
+	typename TEqualFunc = std::equal_to<TKey>,
+	typename TAllocator = std::allocator<TKey>>
+using unordered_set = momo::stdish::unordered_set_open<TKey, THashFunc, TEqualFunc, TAllocator>;
+#include "LibcxxUnorderedSetTests.h"
+#undef LIBCXX_TEST_PREFIX
+
+} // namespace
 
 #endif // TEST_LIBCXX_HASH_SET
