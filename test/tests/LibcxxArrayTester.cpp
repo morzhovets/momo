251bbaebb80224ca51143920d0072f286abfd338
morzhovets <morzhovets@gmail.com>
2019-12-29 17:18:06 +0500

    vector_intcap testing

--- test/tests/LibcxxArrayTester.cpp
+++ test/tests/LibcxxArrayTester.cpp
@@ -54,25 +54,6 @@ using vector = momo::stdish::vector<TValue, TAllocator,
 #undef LIBCXX_TEST_PREFIX
 #undef LIBCXX_TEST_ARRAY
 
-#define LIBCXX_TEST_INTCAP_ARRAY
-#define LIBCXX_TEST_PREFIX "libcxx_test_array_5"
-namespace libcxx_test_array_5
-{
-class LibcxxArraySettings : public momo::ArraySettings<5, false>
-{
-public:
-	static const momo::CheckMode checkMode = momo::CheckMode::exception;
-};
-template<typename TValue,
-	typename TAllocator = std::allocator<TValue>>
-using vector = momo::stdish::vector<TValue, TAllocator,
-	momo::Array<TValue, momo::MemManagerStd<TAllocator>,
-		momo::ArrayItemTraits<TValue, momo::MemManagerStd<TAllocator>>, LibcxxArraySettings>>;
-#include "LibcxxVectorTests.h"
-}
-#undef LIBCXX_TEST_PREFIX
-#undef LIBCXX_TEST_INTCAP_ARRAY
-
 //#undef LIBCPP_TEST_STACK_ALLOCATOR
 
 #define LIBCXX_TEST_SEGMENTED_ARRAY
@@ -119,5 +100,17 @@ using vector = momo::stdish::vector<TValue, TAllocator,
 
 #undef _LIBCPP_DEBUG
 #undef _LIBCPP_DEBUG_LEVEL
+
+#define LIBCXX_TEST_INTCAP_ARRAY
+#define LIBCXX_TEST_PREFIX "libcxx_test_array_5"
+namespace libcxx_test_array_5
+{
+template<typename TValue,
+	typename TAllocator = std::allocator<TValue>>
+using vector = momo::stdish::vector_intcap<5, TValue, TAllocator>;
+#include "LibcxxVectorTests.h"
+}
+#undef LIBCXX_TEST_PREFIX
+#undef LIBCXX_TEST_INTCAP_ARRAY
 
 #endif // TEST_LIBCXX_ARRAY
