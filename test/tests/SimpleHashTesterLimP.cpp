e27375f29c3d0cfe1262c638d4edefae3973a42b
morzhovets <morzhovets@gmail.com>
2020-01-05 19:40:30 +0500

    SimpleHashTester

--- test/tests/SimpleHashTesterLimP.cpp
+++ test/tests/SimpleHashTesterLimP.cpp
@@ -49,20 +49,20 @@ static int testSimpleHash = []
 
 	SimpleHashTester::TestTemplHashSet<BUCKET( 1, 16, true), 11, 1>("momo::HashBucketLimP< 1, 16, true>");
 	SimpleHashTester::TestTemplHashSet<BUCKET( 1, 64, true),  1, 1>("momo::HashBucketLimP< 1, 64, true>");
-	SimpleHashTester::TestTemplHashSet<BUCKET( 2, 99, true),  3, 1>("momo::HashBucketLimP< 2, 99, true>");
+	SimpleHashTester::TestTemplHashSet<BUCKET( 2, 99, true),  4, 1>("momo::HashBucketLimP< 2, 99, true>");
 	SimpleHashTester::TestTemplHashSet<BUCKET( 2, 88, true),  2, 2>("momo::HashBucketLimP< 2, 88, true>");
 	SimpleHashTester::TestTemplHashSet<BUCKET( 2,  1, true),  8, 4>("momo::HashBucketLimP< 2,  1, true>");
-	SimpleHashTester::TestTemplHashSet<BUCKET( 3,  2, true),  6, 1>("momo::HashBucketLimP< 3,  2, true>");
+	SimpleHashTester::TestTemplHashSet<BUCKET( 3,  2, true),  8, 1>("momo::HashBucketLimP< 3,  2, true>");
 	SimpleHashTester::TestTemplHashSet<BUCKET( 3,  3, true),  8, 2>("momo::HashBucketLimP< 3,  3, true>");
 	SimpleHashTester::TestTemplHashSet<BUCKET( 4,  4, true),  4, 4>("momo::HashBucketLimP< 4,  4, true>");
-	SimpleHashTester::TestTemplHashSet<BUCKET( 5,  5, true), 10, 2>("momo::HashBucketLimP< 5,  5, true>");
-	SimpleHashTester::TestTemplHashSet<BUCKET( 6,  8, true),  4, 4>("momo::HashBucketLimP< 6,  8, true>");
+	SimpleHashTester::TestTemplHashSet<BUCKET( 5,  5, true), 16, 2>("momo::HashBucketLimP< 5,  5, true>");
+	SimpleHashTester::TestTemplHashSet<BUCKET( 6,  8, true),  8, 8>("momo::HashBucketLimP< 6,  8, true>");
 	SimpleHashTester::TestTemplHashSet<BUCKET( 7, 11, true), 16, 4>("momo::HashBucketLimP< 7, 11, true>");
-	SimpleHashTester::TestTemplHashSet<BUCKET( 8, 33, true),  8, 8>("momo::HashBucketLimP< 8, 33, true>");
+	SimpleHashTester::TestTemplHashSet<BUCKET( 8, 33, true), 16, 8>("momo::HashBucketLimP< 8, 33, true>");
 	SimpleHashTester::TestTemplHashSet<BUCKET(10, 32, true), 32, 8>("momo::HashBucketLimP<10, 32, true>");
-	SimpleHashTester::TestTemplHashSet<BUCKET(12, 55, true), 11, 1>("momo::HashBucketLimP<12, 55, true>");
-	SimpleHashTester::TestTemplHashSet<BUCKET(14,  1, true), 16, 8>("momo::HashBucketLimP<14,  1, true>");
-	SimpleHashTester::TestTemplHashSet<BUCKET(15, 32, true), 16, 8>("momo::HashBucketLimP<15, 32, true>");
+	SimpleHashTester::TestTemplHashSet<BUCKET(12, 55, true), 32, 1>("momo::HashBucketLimP<12, 55, true>");
+	SimpleHashTester::TestTemplHashSet<BUCKET(14,  1, true), 32, 8>("momo::HashBucketLimP<14,  1, true>");
+	SimpleHashTester::TestTemplHashSet<BUCKET(15, 32, true), 32, 8>("momo::HashBucketLimP<15, 32, true>");
 
 	return 0;
 }();
