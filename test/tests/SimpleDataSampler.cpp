37e677faecde95b445e97a77ca1961e5a764996a
morzhovets <morzhovets@gmail.com>
2020-07-02 23:30:02 +0500

    SimpleDataSampler

--- test/tests/SimpleDataSampler.cpp
+++ test/tests/SimpleDataSampler.cpp
@@ -33,7 +33,8 @@ namespace sample1
 		// construct empty table with 3 columns
 		momo::DataTable<> table(intCol, dblCol, strCol);
 
-		table.AddUniqueHashIndex(strCol, intCol);	// unique index (primary key)
+		// unique index (primary key)
+		table.AddUniqueHashIndex(strCol, intCol);
 
 		table.AddRow(strCol = "b", intCol = 1, dblCol = 0.5);
 		table.AddRow(intCol = 2, dblCol = 0.5);	// strCol = ""
@@ -81,43 +82,54 @@ namespace sample1
 
 namespace sample2
 {
-	struct Struct
-	{
-		int intCol;
-		double dblCol;
-		std::string strCol;
-	};
-
-	MOMO_DATA_COLUMN_STRUCT(Struct, intCol);
-	MOMO_DATA_COLUMN_STRUCT(Struct, dblCol);
-	MOMO_DATA_COLUMN_STRUCT(Struct, strCol);
-
-	using ColumnList = momo::DataColumnListStatic<Struct>;
+	using Struct = momo::DataStructDefault<int, double, std::string>;
+	using ColumnList = momo::DataColumnList<momo::DataColumnTraits<Struct>>;
 	using Table = momo::DataTable<ColumnList>;
+
+	MOMO_DATA_COLUMN_STRING_TAG(Struct, int, intCol);
+	MOMO_DATA_COLUMN_STRING_TAG(Struct, double, dblCol);
+	MOMO_DATA_COLUMN_STRING_TAG(Struct, std::string, strCol);
 
 	void Test()
 	{
-		Table table;
+		// construct empty table with 3 columns
+		Table table(intCol, dblCol, strCol);
 
+		// for fast select
 		table.AddMultiHashIndex(strCol);
 
 		{
-			auto row = table.NewRow();
-			row->intCol = 2;
-			row->dblCol = 0.5;
-			row->strCol = "a";
+			auto row = table.NewRow(intCol = 1, strCol = "b");
+			row[dblCol] = 1.5;
 			table.AddRow(std::move(row));
 		}
 		{
-			auto row = table.NewRow(intCol = 1, strCol = "b");
-			row[dblCol] = 1.5;
-			table.InsertRow(0, std::move(row));	// at position 0
+			auto row = table.NewRow();
+			row[intCol] = 2;
+			row[dblCol] = 0.5;
+			row[strCol] = "a";
+			table.AddRow(std::move(row));
 		}
 
+#if defined(__cpp_generic_lambdas)
 		for (auto row : table)
-			std::cout << row->intCol << "," << row->dblCol << "," << row->strCol << std::endl;
-		// 1,1.5,b
-		// 2,0.5,a
+		{
+			row.VisitReferences([] (auto& item) { std::cout << item << " "; });
+			std::cout << std::endl;
+		}
+		// 1 1.5 b 
+		// 2 0.5 a 
+
+		for (auto row : table)
+		{
+			auto visitor = [] (auto& item, auto column)
+				{ std::cout << column.GetName() << "=" << item << " "; };
+			row.VisitReferences(visitor);
+			std::cout << std::endl;
+		}
+		// intCol=1 dblCol=1.5 strCol=b 
+		// intCol=2 dblCol=0.5 strCol=a 
+#endif
 
 		{
 			auto selection = table.Select(strCol == "a");	// fast select by index
@@ -128,12 +140,13 @@ namespace sample2
 		}
 
 		{
-			auto selection = table.Select(intCol == 2, dblCol == 0.5);	// slow select (takes linear time)
-			std::cout << selection.GetCount() << std::endl;	// 0
+			size_t selCount = table.SelectCount(intCol == 2, dblCol == 0.5);	// slow select (takes linear time)
+			std::cout << selCount << std::endl;	// 0
 		}
 
 #if defined(__cpp_generic_lambdas)
 		{
+			// C++11: (Table::ConstRowReference row)
 			auto selection = table.Select([] (auto row) { return row[dblCol] > 0.0; });	// slow select
 			std::cout << selection.GetCount() << std::endl;	// 1
 		}
@@ -141,6 +154,67 @@ namespace sample2
 		table.RemoveRows([] (auto row) { return row[dblCol] > 1.0; });
 		std::cout << table.GetCount() << std::endl;	// 0
 #endif
+	}
+}
+
+namespace sample3
+{
+	struct Struct
+	{
+		int intCol;
+		double dblCol;
+		std::string strCol;
+	};
+
+	using ColumnList = momo::DataColumnListStatic<Struct>;
+	using Table = momo::DataTable<ColumnList>;
+
+	MOMO_DATA_COLUMN_STRUCT(Struct, intCol);
+	MOMO_DATA_COLUMN_STRUCT(Struct, dblCol);
+	MOMO_DATA_COLUMN_STRUCT(Struct, strCol);
+
+	void Test()
+	{
+		// construct empty table with 3 columns
+		Table table;
+
+		{
+			auto row = table.NewRow();
+			row->intCol = 2;
+			row->dblCol = 0.5;
+			row->strCol = "a";
+			table.AddRow(std::move(row));
+		}
+		{
+			auto row = table.NewRow(intCol = 1, strCol = "a");
+			row[dblCol] = 1.5;
+			table.InsertRow(0, std::move(row));	// at position 0
+		}
+
+		for (auto row : table)
+			std::cout << row->intCol << "," << row->dblCol << "," << row->strCol << std::endl;
+		// 1,1.5,a
+		// 2,0.5,a
+
+		auto uniqueIndex = table.AddUniqueHashIndex(strCol, intCol);
+		auto multiIndex = table.AddMultiHashIndex(strCol);
+
+		{
+			auto rows = table.FindByMultiHash(multiIndex, strCol == "a");	// fastest search
+			std::cout << rows.GetCount() << std::endl;
+			// 2
+		}
+
+		{
+			auto prow = table.FindByUniqueHash(uniqueIndex, intCol == 1, strCol == "a");	// fastest search
+			if (prow)
+				std::cout << (*prow)[dblCol] << std::endl;
+			// 1.5
+		}
+
+		table.RemoveRow(table[0]);
+		std::cout << table[0][dblCol] << std::endl;
+		// 0.5
 	}
 }
 
