320394713fa4b5612d623a382c21483d3ead187b
morzhovets <morzhovets@gmail.com>
2020-10-13 19:51:47 +0500

    Testing deduction guides

--- test/tests/LibcxxTester.h
+++ test/tests/LibcxxTester.h
@@ -22,7 +22,7 @@
 #if !defined(LIBCPP_TEST_DEDUCTION_GUIDES) && defined(_MSC_VER) && _MSC_VER >= 1924
 #define LIBCPP_TEST_DEDUCTION_GUIDES
 #endif
-#if !defined(LIBCPP_TEST_DEDUCTION_GUIDES) && defined(__GNUC__) && __GNUC__ >= 9	// 8
+#if !defined(LIBCPP_TEST_DEDUCTION_GUIDES) && defined(__GNUC__) && __GNUC__ >= 8
 #define LIBCPP_TEST_DEDUCTION_GUIDES
 #endif
 
