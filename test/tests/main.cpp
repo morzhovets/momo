14f6abb96ebe014e01eec960ad45d01f393f577e
morzhovets <morzhovets@gmail.com>
2022-04-24 18:43:19 +0400

    Print compiler versions

--- test/tests/main.cpp
+++ test/tests/main.cpp
@@ -12,6 +12,15 @@
 
 int main()
 {
+#ifdef _MSC_VER
+	std::cout << "_MSC_VER: " << _MSC_VER << std::endl;
+#endif
+#ifdef __GNUC__
+	std::cout << "__GNUC__: " << __GNUC__ << std::endl;
+#endif
+#ifdef __clang_major__
+	std::cout << "__clang_major__: " << __clang_major__ << std::endl;
+#endif
 	std::cout << "__cplusplus: " << __cplusplus << std::endl;
 	std::cout << "sizeof(void*): " << sizeof(void*) << std::endl;
 	return 0;
