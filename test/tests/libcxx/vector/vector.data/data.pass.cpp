f4d5e3013a8d6cad98249083921f429adc783a57
morzhovets <morzhovets@gmail.com>
2022-10-21 22:55:25 +0400

    Update vector tests

--- test/tests/libcxx/vector/vector.data/data.pass.cpp
+++ test/tests/libcxx/vector/vector.data/data.pass.cpp
@@ -1,9 +1,8 @@
 //===----------------------------------------------------------------------===//
 //
-//                     The LLVM Compiler Infrastructure
-//
-// This file is dual licensed under the MIT and the University of Illinois Open
-// Source Licenses. See LICENSE.TXT for details.
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
 
@@ -14,10 +13,20 @@
 //#include <vector>
 //#include <cassert>
 
+//#include "test_macros.h"
 //#include "min_allocator.h"
 //#include "asan_testing.h"
 
-void main()
+struct Nasty {
+    TEST_CONSTEXPR Nasty() : i_(0) {}
+    TEST_CONSTEXPR Nasty(int i) : i_(i) {}
+    TEST_CONSTEXPR_CXX20 ~Nasty() {}
+
+    Nasty * operator&() const { assert(false); return nullptr; }
+    int i_;
+};
+
+TEST_CONSTEXPR_CXX20 bool tests()
 {
     {
         vector<int> v;
@@ -26,20 +35,40 @@ void main()
     }
     {
         vector<int> v(100);
-        assert(v.data() == &v.front());
+        assert(v.data() == std::addressof(v.front()));
         //assert(is_contiguous_container_asan_correct(v));
     }
-//#if __cplusplus >= 201103L
+    {
+        vector<Nasty> v(100);
+        assert(v.data() == std::addressof(v.front()));
+        //assert(is_contiguous_container_asan_correct(v));
+    }
+//#if TEST_STD_VER >= 11
 #ifdef LIBCPP_TEST_MIN_ALLOCATOR
     {
         vector<int, min_allocator<int>> v;
-        assert(v.data() == 0);
+        assert(v.data() == nullptr);
         //assert(is_contiguous_container_asan_correct(v));
     }
     {
         vector<int, min_allocator<int>> v(100);
-        assert(v.data() == &v.front());
+        assert(v.data() == std::addressof(v.front()));
+        //assert(is_contiguous_container_asan_correct(v));
+    }
+    {
+        vector<Nasty, min_allocator<Nasty>> v(100);
+        assert(v.data() == std::addressof(v.front()));
         //assert(is_contiguous_container_asan_correct(v));
     }
 #endif
+
+    return true;
+}
+
+void main()
+{
+    tests();
+//#if TEST_STD_VER > 17
+//    static_assert(tests());
+//#endif
 }
