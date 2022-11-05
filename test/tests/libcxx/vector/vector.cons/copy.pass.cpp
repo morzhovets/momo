27774bbc3c8f5b2cdb41ece4952771e2c6cc7bc1
morzhovets <morzhovets@gmail.com>
2022-11-05 18:09:26 +0400

    Update vector tests

--- test/tests/libcxx/vector/vector.cons/copy.pass.cpp
+++ test/tests/libcxx/vector/vector.cons/copy.pass.cpp
@@ -1,9 +1,8 @@
 //===----------------------------------------------------------------------===//
 //
-//                     The LLVM Compiler Infrastructure
-//
-// This file is dual licensed under the MIT and the University of Illinois Open
-// Source Licenses. See LICENSE.TXT for details.
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
 
@@ -13,29 +12,25 @@
 
 //#include <vector>
 //#include <cassert>
+
+//#include "test_macros.h"
 //#include "test_allocator.h"
 //#include "min_allocator.h"
 //#include "asan_testing.h"
 
 template <class C>
-void
+TEST_CONSTEXPR_CXX20 void
 test(const C& x)
 {
-    size_t s = x.size();
+    typename C::size_type s = x.size();
     C c(x);
-    //assert(c.__invariants());
+    //LIBCPP_ASSERT(c.__invariants());
     assert(c.size() == s);
     assert(c == x);
-    assert(!(c != x));
-    assert(c <= x);
-    assert(c >= x);
-    assert(!(c < x));
-    assert(!(c > x));
-    //assert(is_contiguous_container_asan_correct(c));
+    //LIBCPP_ASSERT(is_contiguous_container_asan_correct(c));
 }
 
-void main()
-{
+TEST_CONSTEXPR_CXX20 bool tests() {
     {
         int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 1, 0};
         int* an = a + sizeof(a)/sizeof(a[0]);
@@ -51,7 +46,19 @@ void main()
         //assert(is_contiguous_container_asan_correct(v));
         //assert(is_contiguous_container_asan_correct(v2));
     }
-#ifndef _LIBCPP_HAS_NO_ADVANCED_SFINAE
+    {
+        // Test copy ctor with empty source
+        vector<int, test_allocator<int> > v(test_allocator<int>(5));
+        vector<int, test_allocator<int> > v2 = v;
+        //assert(is_contiguous_container_asan_correct(v));
+        //assert(is_contiguous_container_asan_correct(v2));
+        assert(v2 == v);
+        assert(v2.get_allocator() == v.get_allocator());
+        //assert(is_contiguous_container_asan_correct(v));
+        //assert(is_contiguous_container_asan_correct(v2));
+        assert(v2.empty());
+    }
+#if TEST_STD_VER >= 11
     {
         vector<int, other_allocator<int> > v(3, 2, other_allocator<int>(5));
         vector<int, other_allocator<int> > v2 = v;
@@ -62,8 +69,6 @@ void main()
         //assert(is_contiguous_container_asan_correct(v));
         //assert(is_contiguous_container_asan_correct(v2));
     }
-#endif  // _LIBCPP_HAS_NO_ADVANCED_SFINAE
-#if TEST_STD_VER >= 11
 #ifdef LIBCPP_TEST_MIN_ALLOCATOR
     {
         int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 1, 0};
@@ -71,7 +76,7 @@ void main()
         test(vector<int, min_allocator<int>>(a, an));
     }
     {
-        vector<int, min_allocator<int> > v((size_t)3, 2, min_allocator<int>());
+        vector<int, min_allocator<int> > v(3, 2, min_allocator<int>());
         vector<int, min_allocator<int> > v2 = v;
         //assert(is_contiguous_container_asan_correct(v));
         //assert(is_contiguous_container_asan_correct(v2));
@@ -82,4 +87,14 @@ void main()
     }
 #endif
 #endif
+
+    return true;
+}
+
+void main()
+{
+    tests();
+//#if TEST_STD_VER > 17
+//    static_assert(tests());
+//#endif
 }
--- test/tests/libcxx/vector/vector.cons/copy_alloc.pass.cpp
+++ test/tests/libcxx/vector/vector.cons/copy_alloc.pass.cpp
@@ -1,9 +1,8 @@
 //===----------------------------------------------------------------------===//
 //
-//                     The LLVM Compiler Infrastructure
-//
-// This file is dual licensed under the MIT and the University of Illinois Open
-// Source Licenses. See LICENSE.TXT for details.
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
 
@@ -13,24 +12,25 @@
 
 //#include <vector>
 //#include <cassert>
+
+//#include "test_macros.h"
 //#include "test_allocator.h"
 //#include "min_allocator.h"
 //#include "asan_testing.h"
 
 template <class C>
-void
+TEST_CONSTEXPR_CXX20 void
 test(const C& x, const typename C::allocator_type& a)
 {
-    size_t s = x.size();
+    typename C::size_type s = x.size();
     C c(x, a);
-    //assert(c.__invariants());
+    //LIBCPP_ASSERT(c.__invariants());
     assert(c.size() == s);
     assert(c == x);
-    //assert(is_contiguous_container_asan_correct(c));
+    //LIBCPP_ASSERT(is_contiguous_container_asan_correct(c));
 }
 
-void main()
-{
+TEST_CONSTEXPR_CXX20 bool tests() {
     {
         int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 1, 0};
         int* an = a + sizeof(a)/sizeof(a[0]);
@@ -48,6 +48,14 @@ void main()
         assert(l2 == l);
         assert(l2.get_allocator() == other_allocator<int>(3));
     }
+    {
+        // Test copy ctor with allocator and empty source
+        vector<int, other_allocator<int> > l(other_allocator<int>(5));
+        vector<int, other_allocator<int> > l2(l, other_allocator<int>(3));
+        assert(l2 == l);
+        assert(l2.get_allocator() == other_allocator<int>(3));
+        assert(l2.empty());
+    }
 #if TEST_STD_VER >= 11
 #ifdef LIBCPP_TEST_MIN_ALLOCATOR
     {
@@ -56,11 +64,21 @@ void main()
         test(vector<int, min_allocator<int>>(a, an), min_allocator<int>());
     }
     {
-        vector<int, min_allocator<int> > l((size_t)3, 2, min_allocator<int>());
+        vector<int, min_allocator<int> > l(3, 2, min_allocator<int>());
         vector<int, min_allocator<int> > l2(l, min_allocator<int>());
         assert(l2 == l);
         assert(l2.get_allocator() == min_allocator<int>());
     }
 #endif
 #endif
+
+    return true;
+}
+
+void main()
+{
+    tests();
+//#if TEST_STD_VER > 17
+//    static_assert(tests());
+//#endif
 }
