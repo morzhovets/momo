6839e5ebdda7abb12c99e3f0a8d22b4146e57a49
morzhovets <morzhovets@gmail.com>
2023-02-26 18:08:38 +0400

    SimpleMemPoolTester

---
+++ test/tests/SimpleMemPoolTester.cpp
@@ -0,0 +1,132 @@
+/**********************************************************\
+
+  This file is part of the
+  https://github.com/morzhovets/momo
+  project, distributed under the MIT License. See
+  https://github.com/morzhovets/momo/blob/master/LICENSE
+  for details.
+
+  tests/SimpleMemPoolTester.cpp
+
+\**********************************************************/
+
+#include "pch.h"
+
+#ifdef TEST_SIMPLE_MEM_POOL
+
+#include "../../momo/MemPool.h"
+#include "../../momo/Array.h"
+
+#include <iostream>
+#include <random>
+
+class SimpleMemPoolTester
+{
+private:
+	class MemManager
+	{
+	public:
+		explicit MemManager() noexcept
+			: mTotalSize(0)
+		{
+		}
+
+		MemManager(MemManager&& memManager) noexcept
+			: mTotalSize(std::exchange(memManager.mTotalSize, 0))
+		{
+		}
+
+		MemManager(const MemManager&) = delete;
+
+		~MemManager() noexcept
+		{
+			assert(mTotalSize == 0);
+		}
+
+		MemManager& operator=(const MemManager&) = delete;
+
+		[[nodiscard]] void* Allocate(size_t size)
+		{
+			void* res = momo::MemManagerDefault().Allocate(size);
+			mTotalSize += size;
+			return res;
+		}
+
+		void Deallocate(void* ptr, size_t size) noexcept
+		{
+			momo::MemManagerDefault().Deallocate(ptr, size);
+			mTotalSize -= size;
+		}
+
+	private:
+		size_t mTotalSize;
+	};
+
+public:
+	static void TestTemplAll()
+	{
+		std::mt19937 mt;
+
+		TestTemplMemPool<1, 1, 1, 0>(mt);
+	}
+
+	template<size_t blockSize, size_t blockAlignment, size_t blockCount, size_t cachedFreeBlockCount>
+	static void TestTemplMemPool(std::mt19937& mt)
+	{
+		std::cout << "momo::MemPoolParamsStatic<" << blockSize << ", " << blockAlignment << ", "
+			<< blockCount << ", " << cachedFreeBlockCount << ">: " << std::flush;
+
+		TestMemPoolParams(mt,
+			momo::MemPoolParamsStatic<blockSize, blockAlignment, blockCount, cachedFreeBlockCount>());
+
+		TestMemPoolParams(mt,
+			momo::MemPoolParams<blockCount, cachedFreeBlockCount>(blockSize, blockAlignment));
+
+		std::cout << "ok" << std::endl;
+	}
+
+	template<typename MemPoolParams>
+	static void TestMemPoolParams(std::mt19937& mt, const MemPoolParams& params)
+	{
+		momo::MemPool<MemPoolParams, MemManager> memPool(params);
+
+		static const size_t blockCount = 1024;
+		static const size_t testCount = 64;
+		momo::Array<void*> blocks = momo::Array<void*>::CreateCap(blockCount);
+
+		for (size_t k = 0; k < testCount; ++k)
+		{
+			while (blocks.GetCount() < blockCount)
+			{
+				blocks.AddBack(memPool.Allocate());
+				std::memset(blocks.GetBackItem(), 0, memPool.GetBlockSize());
+			}
+			assert(memPool.GetAllocateCount() == blockCount);
+
+			std::shuffle(blocks.GetBegin(), blocks.GetEnd(), mt);
+
+			size_t lim = k * blockCount / testCount;
+			while (blocks.GetCount() > lim)
+			{
+				memPool.Deallocate(blocks.GetBackItem());
+				blocks.RemoveBack();
+			}
+			assert(memPool.GetAllocateCount() == lim);
+		}
+
+		if (memPool.CanDeallocateAll())
+		{
+			memPool.DeallocateAll();
+		}
+		else
+		{
+			for (void* block : blocks)
+				memPool.Deallocate(block);
+		}
+		assert(memPool.GetAllocateCount() == 0);
+	}
+};
+
+static int testSimpleMemPool = (SimpleMemPoolTester::TestTemplAll(), 0);
+
+#endif // TEST_SIMPLE_MEM_POOL
