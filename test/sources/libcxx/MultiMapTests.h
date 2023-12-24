91fc560d1b10d078af89d52fce146fb9c0f79ebd
morzhovets <morzhovets@gmail.com>
2023-12-24 18:39:40 +0400

    Update libcxx/multimap

--- test/sources/libcxx/MultiMapTests.h
+++ test/sources/libcxx/MultiMapTests.h
@@ -88,11 +88,9 @@ LIBCXX_TEST_BEGIN(cons_deduct)
 #include "multimap/multimap.cons/deduct.pass.cpp"
 LIBCXX_TEST_END
 
-#ifdef LIBCPP_HAS_BAD_NEWS_FOR_MOMO
 LIBCXX_TEST_BEGIN(cons_deduct_const)
 #include "multimap/multimap.cons/deduct_const.pass.cpp"
 LIBCXX_TEST_END
-#endif
 
 LIBCXX_TEST_BEGIN(cons_default)
 #include "multimap/multimap.cons/default.pass.cpp"
