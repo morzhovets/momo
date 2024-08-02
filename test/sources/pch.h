16c73090efecfccac73ab0a6bbcf157aafdfd323
morzhovets <morzhovets@gmail.com>
2024-08-02 21:00:54 +0400

    Cosmetic changes

--- test/sources/pch.h
+++ test/sources/pch.h
@@ -15,16 +15,16 @@
 #include "TestSettings.h"
 
 #ifdef TEST_MSVC
-#define _SCL_SECURE_NO_WARNINGS
-#define _CRT_SECURE_NO_WARNINGS
-#pragma warning (disable: 4127)	// conditional expression is constant
-#pragma warning (disable: 4458)	// declaration of '...' hides class member
+# define _SCL_SECURE_NO_WARNINGS
+# define _CRT_SECURE_NO_WARNINGS
+# pragma warning (disable: 4127)	// conditional expression is constant
+# pragma warning (disable: 4458)	// declaration of '...' hides class member
 #endif
 
 #ifdef _WIN32
-#define MOMO_USE_MEM_MANAGER_WIN
-#define NOMINMAX
-#include <Windows.h>
+# define MOMO_USE_MEM_MANAGER_WIN
+# define NOMINMAX
+# include <Windows.h>
 #endif
 
 #include "../../include/momo/Utility.h"
