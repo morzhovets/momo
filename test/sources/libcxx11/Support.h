c1c9aca721780d83aad3847a3fbc1b68a0953d6b
morzhovets <morzhovets@gmail.com>
2024-11-03 16:32:06 +0400

    Update libcxx11/Support.h

--- test/sources/libcxx11/Support.h
+++ test/sources/libcxx11/Support.h
@@ -17,20 +17,20 @@
 #include <cmath>
 
 #ifdef __cpp_lib_optional
-#include <optional>
+# include <optional>
 #endif
 
 #if !defined(TEST_MSVC) && !defined(__cpp_lib_transparent_operators)
-#define LIBCPP_HAS_NO_TRANSPARENT_OPERATORS
+# define LIBCPP_HAS_NO_TRANSPARENT_OPERATORS
 #endif
 
 #if !defined(LIBCPP_TEST_DEDUCTION_GUIDES) && defined(MOMO_HAS_DEDUCTION_GUIDES)
-#if defined(TEST_MSVC) && _MSC_VER >= 1924
-#define LIBCPP_TEST_DEDUCTION_GUIDES
-#endif
-#if defined(TEST_GCC) && __GNUC__ >= 8
-#define LIBCPP_TEST_DEDUCTION_GUIDES
-#endif
+# if defined(TEST_MSVC) && _MSC_VER >= 1924
+#  define LIBCPP_TEST_DEDUCTION_GUIDES
+# endif
+# if defined(TEST_GCC) && __GNUC__ >= 8
+#  define LIBCPP_TEST_DEDUCTION_GUIDES
+# endif
 #endif
 
 //#define LIBCPP_HAS_BAD_NEWS_FOR_MOMO
@@ -55,6 +55,12 @@
 #include "support/test_transparent_unordered.h"
 //#include "support/min_allocator.h"
 //#include "support/stack_allocator.h"
+
+namespace libcxx_insert_range_seq {}
+namespace libcxx_from_range_seq {}
+namespace libcxx_insert_range_maps_sets {}
+namespace libcxx_from_range_assoc {}
+namespace libcxx_from_range_unord {}
 
 struct LibcppIntHash
 {
