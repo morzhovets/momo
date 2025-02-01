9116b909d0dd335bd120feb987f33f320b2f18df
morzhovets <morzhovets@gmail.com>
2025-02-01 21:57:29 +0400

    Update CMakeLists

--- test/sources/pch.h
+++ test/sources/pch.h
@@ -14,13 +14,6 @@
 
 #include "TestSettings.h"
 
-#ifdef TEST_MSVC
-# define _SCL_SECURE_NO_WARNINGS
-# define _CRT_SECURE_NO_WARNINGS
-# pragma warning (disable: 4127)	// conditional expression is constant
-# pragma warning (disable: 4458)	// declaration of '...' hides class member
-#endif
-
 #ifdef _WIN32
 # define MOMO_USE_MEM_MANAGER_WIN
 # define NOMINMAX
