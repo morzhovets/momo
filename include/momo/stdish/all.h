89cf0e0a950e30f472b73760f9753dcb5daf3e04
morzhovets <morzhovets@gmail.com>
2025-05-19 00:41:59 +0400

    stdish/all.h

---
+++ include/momo/stdish/all.h
@@ -0,0 +1,24 @@
+/**********************************************************\
+
+  This file is part of the
+  https://github.com/morzhovets/momo
+  project, distributed under the MIT License. See
+  https://github.com/morzhovets/momo/blob/branch_cpp11/LICENSE
+  for details.
+
+  momo/stdish/all.h
+
+\**********************************************************/
+
+#ifndef MOMO_INCLUDE_GUARD_STDISH_ALL
+#define MOMO_INCLUDE_GUARD_STDISH_ALL
+
+#include "vector.h"
+#include "set.h"
+#include "map.h"
+#include "unordered_set.h"
+#include "unordered_map.h"
+#include "unordered_multimap.h"
+#include "pool_allocator.h"
+
+#endif // MOMO_INCLUDE_GUARD_STDISH_ALL
