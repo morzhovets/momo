e295a791e0ab1ed3d8808ff64c60fe63c4bce7a9
morzhovets <morzhovets@gmail.com>
2025-11-27 23:51:42 +0400

    NestedMemPoolSettings

--- include/momo/MemPool.h
+++ include/momo/MemPool.h
@@ -955,7 +955,7 @@ namespace internal
 			typename std::integral_constant<size_t, MemPoolParams::cachedFreeBlockCount>;
 		};
 
-	class NestedMemPoolSettings //: public MemPoolSettings
+	class NestedMemPoolSettings : public MemPoolSettings
 	{
 	public:
 		static const CheckMode checkMode = CheckMode::assertion;
