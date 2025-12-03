7ad703cb77d8bb1262bb1c7e94d9e45fa2cd6f2f
morzhovets <morzhovets@gmail.com>
2025-12-03 21:01:52 +0400

    Update pch.h

--- test/sources/pch.h
+++ test/sources/pch.h
@@ -14,13 +14,41 @@
 
 #include "TestSettings.h"
 
-#include "../../include/momo/Utility.h"
-
 #include <version>
+#include <cstddef>
+#include <cstring>
+#include <cstdlib>
+#include <cstdint>
+#include <memory>
+#include <exception>
+#include <stdexcept>
+#include <algorithm>
+#include <functional>
+#include <utility>
+#include <new>
+#include <iterator>
+#include <type_traits>
+#include <array>
+#include <tuple>
+#include <initializer_list>
+#include <optional>
+#include <concepts>
+#include <compare>
+#include <bit>
+#include <ranges>
 #include <atomic>
+#include <string_view>
+
+#ifndef MOMO_DISABLE_TYPE_INFO
+# include <typeinfo>
+#endif
 
 #ifdef MOMO_USE_MEM_MANAGER_WIN
 # include <Windows.h>
+#endif
+
+#ifdef MOMO_USE_SSE2
+# include <emmintrin.h>
 #endif
 
 #include <iostream>
@@ -32,5 +60,4 @@
 #include <climits>
 #include <cfloat>
 #include <random>
-#include <optional>
 #include <span>
