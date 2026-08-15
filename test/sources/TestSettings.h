ffe5c2aa5fdd717f788d4dbf8ccbd93da11bbd7a
morzhovets <morzhovets@gmail.com>
2026-08-15 23:26:42 +0400

    Update workflows

--- test/sources/TestSettings.h
+++ test/sources/TestSettings.h
@@ -115,9 +115,12 @@
 #  undef TEST_SIMPLE_MEM_POOL	// BitMath
 # endif
 # if _MSC_VER == 1900 && defined(_M_X64)
-#  undef TEST_LIBCXX_HASH_MAP	// libcxx11_hash_map_limp_modifiers_erase_key (without asserts)
+#  undef TEST_LIBCXX_HASH_MAP	// libcxx11_hash_map_limp_modifiers_erase_key (-assert)
 # endif
-# if _MSC_VER >= 1950 && defined(_M_ARM64) && _MSVC_LANG >= 202002L
-#  undef TEST_LIBCXX_ARRAY	// libcxx20_array_5_cons_assign_copy (without asserts)
+# if _MSC_VER == 1929 && !defined(_M_X64) && TEST_LIBCXX_VERSION == 20
+#  undef TEST_LIBCXX_HASH_MAP	// HashSet.h(1291) : fatal error C1001: Internal compiler error (-assert)
+# endif
+# if _MSC_VER == 1951 && defined(_M_ARM64) && TEST_LIBCXX_VERSION == 20
+#  undef TEST_LIBCXX_ARRAY	// libcxx20_array_5_cons_assign_copy (-assert)
 # endif
 #endif
