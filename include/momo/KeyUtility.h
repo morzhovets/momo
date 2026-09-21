0cb1faa500fa6b9efe2a753a56c8723f5a7a2a78
morzhovets <morzhovets@gmail.com>
2026-09-21 23:53:27 +0400

    HashMixer

--- include/momo/KeyUtility.h
+++ include/momo/KeyUtility.h
@@ -107,10 +107,9 @@ namespace internal
 			else
 			{
 				uint64_t hashCode64 = hashCode;
-				// MurmurHash3
-				hashCode64 ^= hashCode64 >> 33;
+				//hashCode64 ^= hashCode64 >> 33;
 				hashCode64 *= 0xFF51AFD7ED558CCDull;
-				hashCode64 ^= hashCode64 >> 33;
+				//hashCode64 ^= hashCode64 >> 33;
 				//hashCode64 *= 0xC4CEB9FE1A85EC53ull;
 				//hashCode64 ^= hashCode64 >> 33;
 				return static_cast<size_t>(hashCode64);
